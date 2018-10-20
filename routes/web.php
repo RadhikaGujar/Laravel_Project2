b2b9ba9c0f357e8d5a5edddcf2fd31126c941dac
Radhika <rg583@njit.edu>
2018-10-20 19:20:54 -0400

    Task :Added named routes to access the pages

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,19 @@
 
 Route::get('/', function () {
     return view('pages.index');
-});
+})->Name('home');
 
 Route::get('/about', function () {
     return view('pages.about');
-});
+})->Name('about');
 
 Route::get('/contact', function () {
     return view('pages.contact');
-});
+})->Name('contact');
 
-Route::post('/contact', function () {
+/*Route::post('/contact', function () {
     $data = request()->all();
+    echo "Name: " . $data['name'] , '<br>';
     echo "Email: " . $data['email'] , '<br>';
     echo "Body: " . $data['body'];
-});
+});*/
